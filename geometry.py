5d786235a5758148bbfc83bb6ebeed813b28a654
Sakshi Karvekar <sakshi7498919478@gmail.com>
2025-12-10 20:22:49 +0530

    geometry

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def area_rect(a,b):
+    print(a*b)
+    
+def perimeter_rect(a,b):
+    print(2*(a+b))
--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import arithmetic as a
+import geometry as geo
+
 print("Hello")
 
 a.sum(10,20)
 a.sub(210,30)
+
+geo.area_rect(10,10)
+geo.perimeter_rect(10,10)
